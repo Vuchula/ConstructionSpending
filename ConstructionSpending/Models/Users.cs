a5230386ff3f719460efe5d8ce8702c201f8e5cc
jvac06 <jvac06@gmail.com>
2020-04-12 15:00:47 -0400

    added required fields to users & usepurpose field

--- ConstructionSpending/Models/Users.cs
+++ ConstructionSpending/Models/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,15 @@ namespace ConstructionSpending.Models
     public class Users
     {
         public int UserID { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
         public string Organization { get; set; }
         public string Industry { get; set; }
+        public string UsePurpose { get; set; }
 
     }
 }
