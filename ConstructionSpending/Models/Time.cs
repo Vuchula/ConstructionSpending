b6156f4fac5ee9bd7dcb22a483653c56fe084cf0
jvac06 <jvac06@gmail.com>
2020-04-11 21:50:54 -0400

    added Time model

---
+++ ConstructionSpending/Models/Time.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionSpending.Models
+{
+    public class Time
+    {
+        public int TimeID { get; set; }
+        public int Year { get; set; }
+        public Quarter Quarter { get; set; }
+        public Month? Month { get; set; }
+    }
+
+    public enum Quarter
+    {
+        Q1 = 1,
+        Q2 = 2,
+        Q3 = 3,
+        Q4 = 4
+    }
+
+    public enum Month
+    {
+        January = 1,
+        February = 2,
+        March = 3, 
+        April = 4,
+        May = 5, 
+        June = 6,
+        July = 7,
+        August = 8, 
+        September = 9,
+        October = 10, 
+        November = 11,
+        December = 12
+    }
+}
