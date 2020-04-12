b4107feec72e0e5fd196f817ed43f25b59ce8fc8
jvac06 <jvac06@gmail.com>
2020-04-11 23:40:56 -0400

    added simple user table

---
+++ ConstructionSpending/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionSpending.Models
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Organization { get; set; }
+        public string Industry { get; set; }
+
+    }
+}
