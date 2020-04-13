18fd66b211f3d27cca1d950ef76bad038339cb31
jvac06 <jvac06@gmail.com>
2020-04-12 22:40:14 -0400

    adding DbContext as a service at startup

--- ConstructionSpending/Startup.cs
+++ ConstructionSpending/Startup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConstructionSpending.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConstructionSpending
 {
@@ -24,6 +26,8 @@ namespace ConstructionSpending
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ResidentialDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ResidentialDataContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
