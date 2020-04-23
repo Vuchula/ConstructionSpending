6e2c5bfb2cfaa8f969108685eea065ae428d34ce
jvac06 <jvac06@gmail.com>
2020-04-23 13:54:00 -0400

    added base 2002 value for Vacancy

--- ConstructionSpending/Controllers/QueryController.cs
+++ ConstructionSpending/Controllers/QueryController.cs
@@ -32,10 +32,10 @@ namespace ConstructionSpending.Controllers
             int endYear = 2019;
             
             //tables used for querying
-            IEnumerable<Time> times = dbContext.Times.ToList();
-            IEnumerable<Spending> spending = dbContext.Spendings.ToList(); //remember in monthly, this one needs conversion
-            IEnumerable<Vacancy> vacancies = dbContext.Vacancies.ToList();
-            IEnumerable<Occupancy> occupancies = dbContext.Occupancies.ToList();
+            //IEnumerable<Time> times = dbContext.Times.ToList();
+            //IEnumerable<Spending> spending = dbContext.Spendings.ToList(); //remember in monthly, this one needs conversion
+            //IEnumerable<Vacancy> vacancies = dbContext.Vacancies.ToList();
+            //IEnumerable<Occupancy> occupancies = dbContext.Occupancies.ToList();
 
             //simple date range
             var simpleRange = dbContext.Times
@@ -43,7 +43,7 @@ namespace ConstructionSpending.Controllers
                 .OrderBy(time => time.Year).ThenBy(time => time.Quarter).ThenBy(time => time.Month)
                 .ToList();
 
-            //query of time range
+            //query of time range with Occup & Vacancy
             var timeRange = dbContext.Times
                 .Where(time => time.Year >= startYear && time.Year <= endYear)
                 .OrderBy(time => time.Year).ThenBy(time => time.Quarter)
@@ -60,7 +60,7 @@ namespace ConstructionSpending.Controllers
 
             return new EmptyResult();
         }
-        public IActionResult Quarter()
+        public IActionResult SpendingToQuarter()
         {
             //query to group values by quarter
             var yearlySpending = dbContext.Spendings
@@ -73,6 +73,37 @@ namespace ConstructionSpending.Controllers
             {
                 Console.WriteLine("Quarter: {0}, Sum: {1:c}", quarter.Quarter + 1, quarter.Sum);
             }
+
+            return new EmptyResult();
+        }
+
+        public IActionResult PercentageChange()
+        {
+            //Using 2000 Q1 as base number we want to calculate the change in
+            //1.Occupied Houses
+            //2.Vacant Houses
+            //3.Total Houses over time
+
+            //declaring the base time = 2000, Q1, month = 0
+            var min = dbContext.Times
+                .OrderBy(time => time.Year).ThenBy(time => time.Quarter).ThenBy(time => time.Month)
+                .Where(time=> time.Year == 2002)
+                .FirstOrDefault();
+            //Console.WriteLine("{0}, {1}, {2}", min.Year, min.Quarter, min.Month);
+
+            //Fetch base value for each Occ, Vac, Spending
+            var baseOccupied = dbContext.Occupancies
+                .Where(value => value.Time == min && value.OccupancyType == (OccupancyType)2)
+                .FirstOrDefault();
+            //Console.WriteLine("{0} {1} {2} {3}", baseOccupied.Time.Year, baseOccupied.Time.Quarter, baseOccupied.Time.Month, baseOccupied.Value);
+
+            var baseVacant = dbContext.Vacancies
+                .Where(value => value.Time == min && value.VacancyType == (VacancyType)2)
+                .FirstOrDefault();
+            Console.WriteLine("{0} {1} {2} {3}", baseVacant.Time.Year, baseVacant.Time.Quarter, baseVacant.Time.Month, baseVacant.Value);
+
+            //Need to make calculations
+            //Repeat the above for each table
 
             return new EmptyResult();
         }
